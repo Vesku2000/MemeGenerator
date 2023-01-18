cebb3f5a2d21ad0fe309468958dbbb592b7867c7
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-01-18 12:22:03 +0200

    update

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function App() {
+    
+    return (
+        <div className="container">
+            <img src="https://picsum.photos/640/360" />
+            <button>Click me</button>
+        </div>
+    )
+}
